75e40fce6f9f9cc6321e52604f0fc0998a3cf696
pjanota <patricjan@live.com>
2018-03-31 00:04:03 +0200

    task 2 & task 3 (draft only) - task 3 is about bug fixing...

--- CodilityUnitTestProj/CustomInvitationTest/Task2.cs
+++ CodilityUnitTestProj/CustomInvitationTest/Task2.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Codility_Free_Trial_Tasks.CustomInvitationTest
 {
@@ -13,21 +16,75 @@ namespace Codility_Free_Trial_Tasks.CustomInvitationTest
         }
 
         [TestMethod]
-        public void Task2_Test_A()
+        public void Task2_Test_Quasi_Constant_3()
         {
-
+            var inputArray = new int[] { 6, 10, 6, 9, 7, 8 };
+            var result = _sut.solution(inputArray);
+            Assert.AreEqual(3, result); // 6,6,7
         }
 
         [TestMethod]
-        public void Task2_Test_B()
+        public void Task2_Test_B_6677()
         {
-
+            var inputArray = new int[] { 6, 6, 7, 7, 8, 10 };
+            var result = _sut.solution(inputArray);
+            Assert.AreEqual(4, result); // 6 6 7 7
         }
 
+        [TestMethod]
+        public void Task2_Test_B_Fool_Me_6677()
+        {
+            var inputArray = new int[] { 6, 7, 7, 8, 8, 10 };
+            var result = _sut.solution(inputArray);
+            Assert.AreEqual(4, result); // 7 7 8 8
+        }
+
+        [TestMethod]
+        public void Task2_Test_B_Empty()
+        {
+            var inputArray = new int[] { };
+            var result = _sut.solution(inputArray);
+            Assert.AreEqual(0, result); // 6 6 7 7
+        }
     }
 
     public class Task2Solution
     {
+        public int solution(int[] A)
+        {
+            var orderedArray = A.OrderBy(e => e).ToArray();
 
+            var subsequenceArray = new List<int>();
+            var rememberSubsequenceLengths = new List<int>();
+            var indexToStore = 0;
+            for (int i = 0; i < orderedArray.Length; i++)
+            {
+                var curElement = orderedArray[i];
+
+                if (subsequenceArray.Count > 0 &&
+                    Math.Abs(subsequenceArray.Min() - curElement) >= 1 && 
+                    indexToStore == 0) // amplitude rise by one => remember
+                {
+                    indexToStore = i;
+                }
+
+                if (subsequenceArray.Count > 0 &&
+                    Math.Abs(subsequenceArray.Min() - curElement) > 1) // amp > 1
+                {
+                    rememberSubsequenceLengths.Add(subsequenceArray.Count); // remember length of subsequence
+                    subsequenceArray.Clear();
+
+                    if (indexToStore != orderedArray.Length - 1)
+                    {
+                        i = indexToStore - 1; // shifting index to where amplitude rise by.1
+                        indexToStore = 0;
+                    }
+                    continue;
+                }
+                subsequenceArray.Add(curElement);
+            }
+            if (rememberSubsequenceLengths.Count == 0) return 0;
+            return rememberSubsequenceLengths.Max();
+        }
     }
 }
--- CodilityUnitTestProj/CustomInvitationTest/Task3.cs
+++ CodilityUnitTestProj/CustomInvitationTest/Task3.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Codility_Free_Trial_Tasks.CustomInvitationTest
 {
@@ -14,19 +15,60 @@ namespace Codility_Free_Trial_Tasks.CustomInvitationTest
         [TestMethod]
         public void Task3_Test_A()
         {
+            var result = _sut.solution(new int[] {1, 1, 0, 1, 0, 0});
 
+            Assert.AreEqual(4, result);
         }
 
         [TestMethod]
         public void Task3_Test_B()
         {
+            var result = _sut.solution(new int[] { 0, 1, 1, 1, 1, 0 });
 
+            Assert.AreEqual(4, result);
         }
 
+        [TestMethod]
+        public void Task3_Test_C()
+        {
+            var result = _sut.solution(new int[] { 1, 0, 0, 1, 1, 0, 1 });
+
+            Assert.AreEqual(4, result);
+        }
     }
 
     public class Task3Solution
     {
-
+        public int solution(int[] A)
+        {
+            int n = A.Length;
+            int result = 0;
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (A[i] == A[i + 1])
+                    result = result + 1;
+            }
+            int r = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int count = 0;
+                if (i > 0)
+                {
+                    if (A[i - 1] != A[i])
+                        count = count + 1;
+                    else
+                        count = count - 1;
+                }
+                if (i < n - 1)
+                {
+                    if (A[i + 1] != A[i])
+                        count = count + 1;
+                    else
+                        count = count - 1;
+                }
+                r = Math.Max(r, count);
+            }
+            return result + r;
+        }
     }
 }
