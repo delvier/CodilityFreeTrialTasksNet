f81e1408a80e059f89b531a87ca92ec2d93beeaa
pljanot <patryk.l.janota@intel.com>
2018-04-03 21:44:29 +0200

    nicole2018 solution.

--- CodilityUnitTestProj/Nicole2018/Nicole2018.cs
+++ CodilityUnitTestProj/Nicole2018/Nicole2018.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Codility_Free_Trial_Tasks.Nicole2018
 {
@@ -29,13 +32,74 @@ namespace Codility_Free_Trial_Tasks.Nicole2018
     //expected worst-case time complexity is O(N);
     //expected worst-case space complexity is O(1), beyond input storage(not counting the storage required for input arguments).
 
+    public class Nicole2018Solution
+    {
+        public double Solution(bool[] P)
+        {
+            if (P.Length == 0)
+            {
+                return 0;
+            }
+
+            long dotCounter = 0;
+            long iterationNumber = P.Length;
+            for (int i = 0; i < iterationNumber; i++)
+            {
+                var currentLevelDotCount = P.Count(e => e);
+                if (currentLevelDotCount == P.Length)
+                {
+                    double filledDot = (double)(1 + (P.Length+1)) * (P.Length+1) / 2; // Sn = (a1+an)*n/2
+                    return dotCounter + filledDot;
+                }
+
+                dotCounter += currentLevelDotCount;
+
+                var nextLevelArray = new List<bool>();
+                for (int j = 0; j < P.Length - 1; j++)
+                {
+                    var result = GetNextLevelElement(P, j);
+                    nextLevelArray.Add(result);
+                }
+                P = nextLevelArray.ToArray();
+            }
+
+            return dotCounter;
+        }
+
+        private bool GetNextLevelElement(bool[] P, int i)
+        {
+            if (i + 1 >= P.Length) // is last element
+            {
+                return false;
+            }
+            else
+            {
+                if (P[i] || P[i + 1])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
     [TestClass]
     public class Nicole2018
     {
         [TestMethod]
+        public void Nicole2018__When_5_Elements_Array_TTTTT__Expected_11_Leafs()
+        {
+            var inputArray = new bool[] { true, true, true, true, true};
+            var nicoleSolution = new Nicole2018Solution();
+            var result = nicoleSolution.Solution(inputArray);
+            Assert.AreEqual(15, result);
+        }
+
+        [TestMethod]
         public void Nicole2018__When_5_Elements_Array_TFFTF__Expected_11_Leafs()
         {
-            var inputArray = new bool[] {true, false, false, true, false};
+            var inputArray = new bool[] { true, false, false, true, false };
             var nicoleSolution = new Nicole2018Solution();
             var result = nicoleSolution.Solution(inputArray);
             Assert.AreEqual(11, result);
@@ -71,18 +135,31 @@ namespace Codility_Free_Trial_Tasks.Nicole2018
         [TestMethod]
         public void Nicole2018__When_Single_False_Element_Array__Expected_0()
         {
-            var inputArray = new bool[] { true };
+            var inputArray = new bool[] { false };
             var nicoleSolution = new Nicole2018Solution();
             var result = nicoleSolution.Solution(inputArray);
             Assert.AreEqual(0, result);
         }
-    }
 
-    public class Nicole2018Solution
-    {
-        public int Solution(bool[] P)
+        [TestMethod]
+        public void Nicole2018__When_Big_True_Array__Expected_Time_Below_100ms()
         {
-            return 0;
+            var maxElements = 100 * 1000;
+            var inputArray = Enumerable.Repeat(true, maxElements).ToArray();
+            var nicoleSolution = new Nicole2018Solution();
+            var result = nicoleSolution.Solution(inputArray);
+            Assert.AreEqual(5000150001, result);
+        }
+
+        [TestMethod]
+        public void Nicole2018__When_Big_True_Array_With_One_False__Expected_Time_Below_100ms()
+        {
+            var maxElements = 100 * 1000;
+            var inputArray = Enumerable.Repeat(true, maxElements).ToArray();
+            inputArray[maxElements - 2] = false;
+            var nicoleSolution = new Nicole2018Solution();
+            var result = nicoleSolution.Solution(inputArray);
+            Assert.AreEqual(5000150001 - 1, result);
         }
     }
 }
